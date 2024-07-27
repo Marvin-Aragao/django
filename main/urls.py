7afdea152854630bf5fc2795f5821f4269d24e93
Marvin-Aragao <marvin_fute@hotmail.com>
2024-07-27 12:13:49 -0300

    Adiciona link para visualizaçao para os alunos

--- main/urls.py
+++ main/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.alunoView, name='aluno_lista'),
+    path('alunoID/<int:id>', views.alunoIDview, name='aluno_detalhe'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,7 +1,11 @@
 from .models import Aluno
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render , get_object_or_404
 
 def alunoView(request):
     aluno_lista = Aluno.objects.all()
     return render(request, 'main/aluno.html',{'aluno_lista':aluno_lista})
+
+def alunoIDview(request, id):
+    aluno = get_object_or_404(Aluno, pk = id)
+    return render(request, 'main/alunoID.html', {'aluno':aluno})
